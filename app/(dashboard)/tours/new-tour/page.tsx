96d6142d56d705a7c78b5570a5ae6e458f85e804
rzounlom <rzounlom@gmail.com>
2025-02-22 06:32:41 -0500

    create action to generate new tour with info

--- app/(dashboard)/tours/new-tour/page.tsx
+++ app/(dashboard)/tours/new-tour/page.tsx
@@ -4,11 +4,13 @@ import {
   dehydrate,
 } from "@tanstack/react-query";
 
+import NewTour from "../../../../components/tour/NewTour";
+
 export default async function NewToursPage() {
   const queryClient = new QueryClient();
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
-      <div>NewToursPage</div>
+      <NewTour />
     </HydrationBoundary>
   );
 }
